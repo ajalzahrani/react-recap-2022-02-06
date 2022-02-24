e4083a829aa696ab06c051e3f24b63ef4dfd26e0
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2022-02-24 19:29:55 +0300

    update about author

--- src/functionbased/pages/SinglePage.js
+++ src/functionbased/pages/SinglePage.js
@@ -16,7 +16,7 @@ const SinglePage = () => {
       slug: "about-author",
       title: "About the Author",
       description:
-        "This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.",
+        "This app was developed by Abdulrahman Alzahrani, just started to learn react. I followed tutorial in https://ibaslogic.com which i recommand to new comers.",
     },
   ]
 
